a5dd599be7aa5c6ad7c1a3c94827fb809a0222f8
Jeison Higuita <jeisonhs93@gmail.com>
2020-10-18 23:11:49 +0200

    connect mutation with dynamoDB

--- api/src/graphql/resolvers/mutations/experience.ts
+++ api/src/graphql/resolvers/mutations/experience.ts
@@ -1,4 +1,25 @@
 import { IExperience, IExperienceMutationArgs } from "../typings";
+import AWS from "aws-sdk";
+
+AWS.config.update({ region: "eu-west-1" });
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const TableName = process.env.EXPERIENCE_TABLE;
+
+async function post(data) {
+  const record = await dynamoDB
+    .put({
+      TableName,
+      Item: {
+        ...data,
+        timestamp: new Date().getTime(),
+      },
+    })
+    .promise();
+
+  console.log("record", record);
+
+  return data;
+}
 
 async function postExperienceMutation(
   _: any,
@@ -8,6 +29,11 @@ async function postExperienceMutation(
     input: { userId, energy },
   } = args;
 
+  try {
+    await post({ user_id: userId, energy });
+  } catch (error) {
+    console.log("this is the error", error);
+  }
   console.log(
     `Mutation with inputs firstInput=${userId} and secondInput=${energy}`
   );
