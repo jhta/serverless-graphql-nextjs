838d9aa717591916c83574cc0f4aca418c40dce4
Jeison Higuita <jeisonhs93@gmail.com>
2020-10-28 20:22:33 +0100

    add distribution to nextjs

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,44 @@
 import React from 'react';
 import Head from 'next/head';
 
+import { Radar } from '@reactchartjs/react-chart.js'
+
+const data = {
+  labels: ['Thing 1', 'Thing 2', 'Thing 3', 'Thing 4', 'Thing 5', 'Thing 6'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [2, 9, 3, 5, 2, 3],
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgba(255, 99, 132, 1)',
+      borderWidth: 1,
+    },
+  ],
+}
+
+const options = {
+  scale: {
+    ticks: { beginAtZero: true },
+  },
+}
+
+const RadarChart = () => (
+  <>
+    <div className='header'>
+      <h1 className='title'>Radar Chart</h1>
+      <div className='links'>
+        <a
+          className='btn btn-gh'
+          href='https://github.com/jerairrest/react-chartjs-2/blob/react16/example/src/charts/Radar.js'
+        >
+          Github Source
+        </a>
+      </div>
+    </div>
+    <Radar data={data} options={options} type="radar" />
+  </>
+)
+
 const Index = () => {
   const title: string = 'Home';
 
@@ -11,7 +49,7 @@ const Index = () => {
       </Head>
       <div>
         <h1>{title}</h1>
-        <img src='/images/bg1.png' alt={'example image'} width={500} />
+        <RadarChart />
       </div>
     </>
   );
